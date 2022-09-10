5cf36e9c3b2b69941e3702a42c9679cd26826170
girish <girishkrkr56@gmail.com>
2022-09-10 22:06:39 +0530

    initial commit

---
+++ src/main/java/com/example/demo/Assignment1Application.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.io.File;
+import java.util.Set;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1Application.class, args);
+		File folder = new File("C:\\Users\\pushp\\Desktop\\girish kr\\java\\java full course");
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+		  if (listOfFiles[i].isFile()) {
+		    System.out.println("File " + listOfFiles[i].getName());
+		  } else if (listOfFiles[i].isDirectory()) {
+		    System.out.println("Directory " + listOfFiles[i].getName());
+		  }
+		}
+	}
+
+}
